43ecf5a536cbe93ffa7614e5f94c69582fc06204
saya-nel <pablobellotw@gmail.com>
2020-10-07 15:46:46 +0200

    Ajout classe de base.

---
+++ src/deployment/CVM.java
@@ -0,0 +1,12 @@
+package deployment;
+
+import fr.sorbonne_u.components.cvm.AbstractCVM;
+
+public class CVM extends AbstractCVM {
+
+	public CVM() throws Exception {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+}
