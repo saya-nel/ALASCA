04876b826d55544832cc99b0d2048b002f15f8a1
Sacha Memmi <5996895-MemmiS@users.noreply.gitlab.com>
2021-01-06 13:06:42 +0100

    Rédaction javadoc MIL_Electricity model en cours

--- src/main/java/simulation/petrolGenerator/PetrolGeneratorElectricity_MILModel.java
+++ src/main/java/simulation/petrolGenerator/PetrolGeneratorElectricity_MILModel.java
@@ -20,58 +20,150 @@ import main.java.simulation.petrolGenerator.events.IsTurnedOn;
 import main.java.simulation.petrolGenerator.events.TurnOff;
 import main.java.simulation.petrolGenerator.events.TurnOn;
 
+/**
+ * The class <code>PetrolGeneratorElectricity_MILModel</code> defines a MIL model
+ * of the electricity consumption of a Washer.
+ * <p><string>Description</string></p>
+ * <p>
+ *     The petrol generator can be switched on/off and it changes the consumption.
+ * </p>
+ * </p>
+ */
 @ModelExternalEvents(imported = { TurnOff.class, AddPetrol.class, GetPetrolLevel.class, IsTurnedOn.class, TurnOn.class,
 		GetMaxLevel.class })
 public class PetrolGeneratorElectricity_MILModel extends AtomicHIOA {
 
 	private static final long serialVersionUID = 1L;
-
+	/** energy generated 			 												*/
 	protected static final double GENERATING = 5;
-
+	/** tension same for all the house 												*/
 	public static final double TENSION = 220;
-
+	/** current intensity production in Amperes; intensity is power/tension. 		*/
 	@ExportedVariable(type = Double.class)
 	protected final Value<Double> currentProduction = new Value<>(this, 0.0, 0);
-
-	// TODO : modeliser la consommation en petrol
-
+	/** current petrol level in liter												*/
 	protected float currentPetrolLevel = 0;
-
+	/** maximum capacity of the reservoir 											*/
 	protected float maximumPetrolLevel = 50; // 50 liters max
-
+	/** true when the electricity consumption of the washer has
+	 * changed after executing an external event (when
+	 * <code>currentState</code> changes 											*/
 	protected boolean consumptionHasChanged = false;
-
+	/** indicates whether the petrol generator is on								*/
 	protected boolean isOn = false;
 
+	/**
+	 * Create a PetrolGenerator MIL model instance.
+	 * <p><strong>Contract</strong></p>
+	 *
+	 * <pre>
+	 *     pre 	true //no precondition
+	 *     post	true // no postcondition
+	 * </pre>
+	 * @param uri					URI of the model.
+	 * @param simulatedTimeUnit		time unit used for the simulation time.
+	 * @param simulationEngine		simulation engine to which the model is attached.
+	 * @throws Exception
+	 */
 	public PetrolGeneratorElectricity_MILModel(String uri, TimeUnit simulatedTimeUnit, SimulatorI simulationEngine)
 			throws Exception {
 		super(uri, simulatedTimeUnit, simulationEngine);
 	}
 
+	/**
+	 * return the maximum capacity of the reservoir of the component PetrolGenerator.
+	 *
+	 * <p><strong>Contract</strong></p>
+	 * <pre>
+	 *     pre 	true 	// no precondition
+	 *     post	{@code ret != null}
+	 * </pre>
+	 * @return	the maximum capacity of the reservoir.
+	 */
 	public float getMaximumPetrolLevel() {
 		return maximumPetrolLevel;
 	}
 
+	/**
+	 * return the current level of the reservoir of the component PetrolGenerator.
+	 *
+	 * <p><strong>Contract</strong></p>
+	 * <pre>
+	 *     pre 	true 	// no precondition
+	 *     post	{@code ret != null}
+	 * </pre>
+	 * @return	the current level of the reservoir.
+	 */
 	public float getCurrentPetrolLevel() {
 		return currentPetrolLevel;
 	}
 
+	/**
+	 *
+	 * <p><strong>Contract</strong></p>
+	 * <pre>
+	 *     pre 	true 	// no precondition
+	 *     post	{@code ret != null}
+	 * </pre>
+	 * @return	true if the PetrolGenerator is on.
+	 */
 	public boolean getIsOn() {
 		return isOn;
 	}
 
+	/**
+	 * switch on the petrol generator
+	 * <p><strong>Contract</strong></p>
+	 * <pre>
+	 *     pre 	true 	// no precondition
+	 *     post	{@code ret != null}
+	 * </pre>
+	 * @return
+	 */
 	public void turnOn() {
 		isOn = true;
 	}
 
+	/**
+	 * switch off the petrol generator
+	 * <p><strong>Contract</strong></p>
+	 * <pre>
+	 *     pre 	true 	// no precondition
+	 *     post	{@code ret != null}
+	 * </pre>
+	 * @return
+	 */
 	public void turnOff() {
 		isOn = false;
 	}
 
+	/**
+	 * toggle the value of the state of the model telling whether the
+	 * electricity consumption level has just changed or not; when it changes
+	 * after receiving an external event, an immediate internal transition
+	 * is triggered to update the level of electricity consumption.
+	 *
+	 * <p><strong>Contract</strong></p>
+	 *
+	 * <pre>
+	 * pre	true		// no precondition.
+	 * post	true		// no postcondition.
+	 * </pre>
+	 *
+	 */
 	public void toggleConsumptionHasChanged() {
 		this.consumptionHasChanged = (this.consumptionHasChanged) ? false : true;
 	}
 
+	/**
+	 * add one liter to the reservoir
+	 * <p><strong>Contract</strong></p>
+	 * <pre>
+	 *     pre 	{@code this.getCurrentPetrolLevel()+1 <= this.getMaximumPetrolLevel()}
+	 *     post	{@code ret != null}
+	 * </pre>
+	 * @return
+	 */
 	public void addOneLPetrol() {
 		this.currentPetrolLevel += 1;
 	}
